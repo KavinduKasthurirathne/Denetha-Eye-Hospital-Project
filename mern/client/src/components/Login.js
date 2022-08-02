5fc99742880a8ad77872d19f051b63230bc80a4a
it21047824 <it21047824@my.sliit.lk>
2022-08-02 16:12:13 +0530

    Created react app and login page

---
+++ mern/client/src/components/Login.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import '../App.css';
+
+export const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const logo = require('../image/denethaLogo.png');
+
+    const handleSubmit = () => {
+        alert(`This doesnt do anything yet
+            entered inputs,
+            username - ${username}
+            password - ${password}`);
+    };
+
+    const usenameOnChange = ({target}) => {
+        setUsername(target.value);
+    }
+    const passwordOnChange = ({target}) => {
+        setPassword(target.value);
+    }
+
+    return (
+        <div className='round-edge-div'>
+            <br />
+            <img className='logo-img' src={logo} alt={'logo'} />
+            <br />
+            <h3>Login</h3>
+            <input name={'username'} type={'text'} value={username} placeholder={'Username'} onChange={usenameOnChange} />
+            <br />
+            <input name={'password'} type={'password'} value={password} placeholder={'Password'} onChange={passwordOnChange} />
+            <br />
+            <button className='button' onClick={handleSubmit}>Sign in</button>
+        </div>
+    );
+}
