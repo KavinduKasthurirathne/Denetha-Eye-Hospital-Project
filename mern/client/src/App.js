d1c425ab9713695f54ffb27ad5e9599bb334342c
IT21077692 - Kasthurirathne K. K. I <it21077692@my.sliit.lk>
2022-08-04 14:35:53 +0530

    patient.js

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Login} from './components/Login'
+import {Patients} from './components/Patients'
 import './App.css';
 
 export const App = () => {
@@ -7,6 +8,7 @@ export const App = () => {
   return (
     <div className="App">
       <Login />
+      <Patients />
     </div>
   );
 };
