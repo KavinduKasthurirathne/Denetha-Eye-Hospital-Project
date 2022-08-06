c7fbb8ab936cadda39cc95990f196ad45cb79f7b
IT21077692 - Kasthurirathne K. K. I <it21077692@my.sliit.lk>
2022-08-06 11:26:35 +0530

    patient page

--- mern/client/src/components/Patients.js
+++ mern/client/src/components/Patients.js
@@ -1,17 +1,47 @@
-import React, { useState } from 'react';
+import '../Patients.css';
 import '../App.css';
+import {Header} from './Header.js';
 
 export const Patients = () => {
 
     return (
-        <div className='table1'>
-            <table>
-                <th>
-                    <td>Name</td>
-                    <td>Age</td>
-                    <td>Contact</td>
-                </th>
+        <>
+        <Header />
+        <h1 class='head'><u>Patient Details</u></h1>
+        <div class='patienttable'>
+            <table class='table1'>
+                <tr>
+                    <th>Patient ID</th>
+                    <th>Patient Name</th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td>1000</td>
+                    <td>Tharushi Perera</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>1001</td>
+                    <td>Thrinith Fernando</td>
+                </tr>
+                <tr>
+                    <td>1002</td>
+                    <td>Lahiru Madushan</td>
+                </tr>
+                <tr>
+                    <td>1003</td>
+                    <td>Samitha Dissanayake</td>
+                </tr>
+                <tr>
+                    <td>1004</td>
+                    <td>Mohommad Nawaz</td>
+                </tr>
+                <tr>
+                    <td>1005</td>
+                    <td>Surith Arawwala</td>
+                </tr>
             </table>
         </div>
+        </>
     );
 }
