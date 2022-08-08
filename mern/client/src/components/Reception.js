84f09423fb70d4b02a3544f7034b8ac88441bd08
Surith <surith1234@gmail.com>
2022-08-08 18:18:38 +0530

    reception updated

--- mern/client/src/components/Reception.js
+++ mern/client/src/components/Reception.js
@@ -1,12 +1,20 @@
 import {React} from 'react';
+import '../App.css';
+import '../Reception.css';
 
 export const Reception = () => {
 
     return (
 
-            
-            <div><center>Reception</center></div>
-
-       
+            <div>
+                
+                <input className='input' id='searchBox'></input>
+                <button className='button' id='PatientSearch'>Patient Search</button>
+                
+                <button className='button' id='newPatient'>Add New Patient +</button>
+                    
+                
+                    
+            </div>
     );
 }
