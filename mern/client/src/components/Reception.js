bf63d5cd0071080c7f8d76e75658ad3af698408f
Surith <surith1234@gmail.com>
2022-08-08 16:25:48 +0530

    updated

--- mern/client/src/components/Reception.js
+++ mern/client/src/components/Reception.js
@@ -3,13 +3,10 @@ import {React} from 'react';
 export const Reception = () => {
 
     return (
-        <>
-<<<<<<< HEAD
+
             
             <div><center>Reception</center></div>
-=======
-            <div className='basic'>Receptionist</div>
->>>>>>> 03d0d799ed3875ee4e8a176c7dd3772a261a3747
-        </>
+
+       
     );
 }
