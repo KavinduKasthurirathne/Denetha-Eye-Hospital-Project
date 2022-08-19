842a82b58996df5db9ca8811959601559910d9da
IT21085130 <it21085130@my.sliit.lk>
2022-08-19 23:43:42 +0530

    staff details table

---
+++ mern/client/src/components/StaffDetails.js
@@ -0,0 +1,22 @@
+import React from 'react';
+function App(){
+    return(
+        <div class = "staffDetails">
+            <input type = "text" placeholder="Search"></input> 
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Job Status</th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><button type = "button">view</button></td>
+                </tr>
+            </table>
+        </div>
+    );
+}
