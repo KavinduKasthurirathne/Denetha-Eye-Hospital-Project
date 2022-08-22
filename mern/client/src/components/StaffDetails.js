dfbea063b22da8bccd57c19a30a5aad25c2c32e1
IT21085130 <it21085130@my.sliit.lk>
2022-08-22 14:44:38 +0530

    meeting table

--- mern/client/src/components/StaffDetails.js
+++ mern/client/src/components/StaffDetails.js
@@ -2,7 +2,11 @@ import React from 'react';
 function App(){
     return(
         <div class = "staffDetails">
+            <h2>Staff Details</h2>
+            <input type = "button" id = "SDB1">Meetings</input>
+            
             <input type = "text" placeholder="Search"></input> 
+            <input type = "button" id = "SDB2" >ADD</input>
             <table>
                 <tr>
                     <th>ID</th>
@@ -14,7 +18,7 @@ function App(){
                     <td></td>
                     <td></td>
                     <td></td>
-                    <td><button type = "button">view</button></td>
+                    <td><button type = "button"><a href = "Profile.js">view</a></button> </td>
                 </tr>
             </table>
         </div>
