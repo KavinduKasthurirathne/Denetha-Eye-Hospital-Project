53391296868117f74c13738d0589608e6142aebd
surith <surith1234@gmail.com>
2022-09-19 13:44:56 +0530

    pointer added

--- mern/client/src/components/Receptionist/SearchResult.js
+++ mern/client/src/components/Receptionist/SearchResult.js
@@ -2,8 +2,6 @@ import {React} from 'react';
 
 export const SearchResult = (props) => {
 
-
-
     return(
         <div className="patientdetails">Patient Name : {props.Pname}<br /><br />
         <i className="fas fa-phone"> </i> : {props.Pno}
