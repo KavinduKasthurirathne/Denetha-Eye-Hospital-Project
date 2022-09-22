f98645a68b313543142036e73551cdbcf89f4d60
surith <surith1234@gmail.com>
2022-09-22 19:23:57 +0530

    Appointment selection completed

--- mern/client/src/components/Receptionist/AppointType.js
+++ mern/client/src/components/Receptionist/AppointType.js
@@ -1,22 +1,31 @@
-import {React} from 'react';
+import React, { useState } from "react";
 import './Reception.css';
 
-export const AppointType = () => {
+export const AppointType = (props) => {
 
+   
+      
+    
    
     
     return(<>
     <div id='appointType'>
         <h2 >Select Appointment type</h2>        
         <table align='center' className='AppointTable'>
-            <tr>
-                <td id='type'><i className="fas fa-calendar-plus"></i><br/>OPD Appoinment</td>
-                <td id='type'><i className="fas fa-calendar-plus"></i><br/>Cilnic Appoinment</td>
-                <td id='type'><i className="fas fa-calendar-plus"></i><br/>Operation Appoinment</td>
-                <td id='type'><i className="fas fa-calendar-plus"></i><br/>Post-operation Appoinment</td>
-            </tr>
+         <tr>
+           <button id="type"><td id="td" onClick={()=>{props.type("O")}}><i className="fas fa-calendar-plus"></i><br/>OPD Appointment</td></button>
+           <button id="type"><td id='td' onClick={()=>{props.type("C")}}><i className="fas fa-calendar-plus"></i><br/>Cilnic Appointment</td></button>
+           <button id="type"><td id='td' onClick={()=>{props.type("S")}}><i className="fas fa-calendar-plus"></i><br/>Surgery Appointment</td></button>
+           <button id="type"><td id='td' onClick={()=>{props.type("P")}}><i className="fas fa-calendar-plus"></i><br/>Post-operation Appointment</td></button>
+         </tr>
         </table>
-        <a href="/"> <button className='button' id='makeAppoint' >Make Appointment</button> </a>
+       
+        <button className='button' id='makeAppoint' onClick={()=>{
+                 props.btnsetter(!props.btnstate)
+            
+                }}>
+            
+            Make Appointment</button>
 
      </div> 
         
