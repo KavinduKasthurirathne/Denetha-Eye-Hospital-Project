34c550c87185c9b8ab3360388ee59abda122b4a5
Asini Madushka <it21078514@my.sliit.lk>
2022-09-25 17:48:28 +0530

    SurgeryTable

---
+++ mern/client/src/components/Surgery/AddnewPatient.js
@@ -0,0 +1,99 @@
+import React, {useState} from "react";
+import {Header} from '../Header.js';
+import axios  from "axios";
+
+export default function AddSuPatient (){
+        const [pnumber , setPnumber]= useState("");
+        const [pname , setName]= useState("");
+        const [number , setNumber]= useState("");
+        const [age , setAge]= useState("");
+        const [gender , setGender]= useState("");
+        const [Stype , setStype]= useState("");
+
+    function sendData(e){
+        e.preventDefault();
+        
+        const newPatient = {
+            pnumber,pname,number,age,gender,Stype
+        }
+        
+        axios.post("http://localhost:5000/api/surgery/add" , newPatient).then(()=> {
+            alert("Patient Details Added")
+            setPnumber("");
+            setName("");
+            setNumber("");
+            setAge("");
+            setGender("");
+            setStype("");
+
+        }).catch((err) => {
+            alert(err)
+        })
+
+    }
+
+
+    return(
+        <div className="container">
+        <form onSubmit = {sendData}>
+            <div className="form-group">
+                <label for="pnumber">Patient Number</label>
+                <input type="number" class="form-control" id="pnumber"  placeholder="Enter Next Number" onChange={(e)=>{
+
+                    setPnumber(e.target.value);
+                }}/>    
+            </div>
+
+            <div className="form-group">
+                <label for="pname">Patient Name</label>
+                <input type="text" class="form-control" id="pname"  placeholder="Enter Patient's Name" onChange={(e)=>{
+
+                setName(e.target.value);
+                }}/>    
+            </div>
+
+            <div className="form-group">
+                <label for="number">Number</label>
+                <input type="number" class="form-control" id="number"  placeholder="Enter Number"  onChange={(e)=>{
+
+                setNumber(e.target.value);
+                }}/>    
+            </div>
+
+            <div class="form-group">
+                <label for="age">Patient Age</label>
+                <input type="number" class="form-control" id="age" placeholder="Enter Age"  onChange={(e)=>{
+
+                setAge(e.target.value);
+                }}/>
+            </div>
+
+            <div className="form-group">
+                <label for="gender">Gender</label>
+                <input type="text" class="form-control" id="gender"  placeholder="Enter Patient's Surgery Type"   onChange={(e)=>{
+
+                        setGender(e.target.value);
+                        }}/>    
+            </div>
+            
+            
+
+            <div className="form-group">
+                <label for="Stype">Surgery Type</label>
+                <input type="text" class="form-control" id="Stype"  placeholder="Enter Patient's Surgery Type"   onChange={(e)=>{
+
+                        setStype(e.target.value);
+                        }}/>    
+            </div>
+            
+            <button type="submit" class="button">Add Patient</button>
+            </form>
+
+
+            </div>
+
+    )
+
+
+
+}
---
+++ mern/client/src/components/Surgery/SurgeryDetails.js
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from "react";
+import {useNavigate} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import './SurgeryDetails.css';
+
+ 
+const Record = (props) => (
+ <tr className="design">
+   <td>{props.record.pnumber}</td>
+   <td>{props.record.pname}</td>
+   <td>{props.record.number}</td>
+   <td>{props.record.age}</td>
+   <td>{props.record.gender}</td>
+   <td>{props.record.Stype}</td>
+   <td> <Link style={{color: 'brown'}} className="btnLink" to={`/edit/${props.record._id}`}>Admission Form</Link></td>
+   <td> <Link style={{color: 'brown'}} className="btnLink" to={`/edit/${props.record._id}`}>Ultra Sound Repot</Link></td>
+   <td> <Link style={{color: 'brown'}} className="btnLink" to={`/edit/${props.record._id}`}>Details Recipt</Link></td>
+   <td><button className="btnLink">Waiting</button></td>
+   <td><button className="btnLink">Paid</button></td>
+   <td>
+     <Link style={{color: 'Black'}} className="btnLink" to={`/edit/${props.record._id}`}>Update</Link> | 
+     <button className="btnLink"
+       onClick={() => {
+         props.deleteRecord(props.record._id);
+       }}
+     >
+       Delete
+     </button>
+   </td>
+ </tr>
+);
+ 
+export default function RecordList() {
+    const navigate = useNavigate();
+    const navigateToAddNew = () => {
+        navigate('/addNew');
+      };
+ const [records, setRecords] = useState([]);
+ 
+ // This method fetches the records from the database.
+ useEffect(() => {
+   async function getRecords() {
+     const response = await fetch(`http://localhost:5000/api/surgery/`);
+ 
+     if (!response.ok) {
+       const message = `An error occurred: ${response.statusText}`;
+       window.alert(message);
+       return;
+     }
+ 
+     const records = await response.json();
+     setRecords(records);
+   }
+ 
+   getRecords();
+ 
+   return;
+ }, [records.length]);
+ 
+ // This method will delete a record
+ async function deleteRecord(id) {
+   await fetch(`http://localhost:5000/api/surgery/delete/${id}`, {
+     method: "DELETE"
+   });
+ 
+   const newRecords = records.filter((el) => el._id !== id);
+   setRecords(newRecords);
+ }
+ 
+ // This method will map out the records on the table
+ function recordList() {
+   return records.map((record) => {
+     return (
+       <Record
+         record={record}
+         deleteRecord={() => deleteRecord(record._id)}
+         key={record._id}
+       />
+     );
+   });
+ }
+ 
+ // This following section will display the table with the records of individuals.
+ return (
+   <div className="container">
+    <h2 style={{marginLeft:20}}>Surgery Details</h2>
+    <button className="button" onClick= {navigateToAddNew}>Add New Patient +</button>
+     <table className="table_table-striped" style={{ marginTop: 20 }}>
+       <thead>
+         <tr>
+           <th className="cwidth">Patient Number</th>
+           <th>Patient Name</th>
+           <th className="cwidth">Number</th>
+           <th className="cwidth">Age</th>
+           <th className="cwidth">Gender</th>
+           <th>Surgery Type</th>
+           <th>Addmission Form</th>
+           <th>Ultra Sound Report</th>
+           <th>Diagnosis Card</th>
+           <th>Theater Details</th>
+           <th>Payment</th>
+           <th>Action</th>
+         </tr>
+       </thead>
+       <tbody>{recordList()}</tbody>
+     </table>
+   </div>
+ );
+}
