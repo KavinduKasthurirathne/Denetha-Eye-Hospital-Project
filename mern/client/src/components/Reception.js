b3fc1f1921d6fcd581d1497f86738b28c710456e
surith <surith1234@gmail.com>
2022-09-26 22:30:30 +0530

    space optimized

--- mern/client/src/components/Reception.js
+++ mern/client/src/components/Reception.js
@@ -9,42 +9,39 @@ import { EditReceipt } from "./Receptionist/EditReceipt";
 
 export const Reception = () => {
 
-    const [NewAppoinment, setNewAppoinment] = useState(false);
     const [selectedName, setSelectedName] = useState("");
     const [selectedPhone, setSelectedPhone] = useState("");
     const [AppoinmentType, setAppoinmentType] = useState("");
     const [NewReceipt, setNewReceipt] = useState(false);
     const [editReceipt, seteditReceipt] = useState(false);
-
+    const [NewAppoinment, setNewAppoinment] = useState(false);
 
     return (
 
-            <div>
-                {!NewAppoinment?<div>
-                    <h1 id='leftText'>Reception</h1>
-                    
-                    <SearchBar selectedName={selectedName} 
-                               setSelectedName={setSelectedName} 
-                               selectedPhone={selectedPhone} 
-                               setSelectedPhone={setSelectedPhone}/>
-                    
-                    {/* <button className='button' id='PatientSearch'>Patient Search &nbsp; <i class="fas fa-search"></i></button> */}
-                    
-                    <a href="/AddPatientForm"> <button className='button' id='newPatient' >Add New Patient &nbsp; <i className="fas fa-plus-circle"></i></button> </a>
-                    
-                    <AppointType btnsetter={setNewAppoinment} btnstate={NewAppoinment} type={setAppoinmentType}/>
-                    
-                </div> : !NewReceipt?
-                    <Appoinment btnsetter ={setNewReceipt} btnstate={NewReceipt} name={selectedName} phone={selectedPhone} type={AppoinmentType}/>
-                    :!editReceipt?
-                        <Receipt btnsetter ={seteditReceipt} btnstate={editReceipt} name={selectedName} phone={selectedPhone} type={AppoinmentType}/>
-                        :<EditReceipt name={selectedName} phone={selectedPhone} type={AppoinmentType}/>
-                }
+        <div>
+            {!NewAppoinment?<div>
+                <h1 id='leftText'>Reception</h1>
                 
+                <SearchBar selectedName={selectedName} 
+                            setSelectedName={setSelectedName} 
+                            selectedPhone={selectedPhone} 
+                            setSelectedPhone={setSelectedPhone}/>
+                                    
+                <a href="/AddPatientForm"> <button className='button' id='newPatient' >Add New Patient &nbsp; <i className="fas fa-plus-circle"></i></button> </a>
+                
+                <AppointType btnsetter={setNewAppoinment} btnstate={NewAppoinment} type={setAppoinmentType}/>
+                
+            </div> : !NewReceipt?
+                <Appoinment btnsetter ={setNewReceipt} btnstate={NewReceipt} name={selectedName} phone={selectedPhone} type={AppoinmentType}/>
+                :!editReceipt?
+                    <Receipt btnsetter ={seteditReceipt} btnstate={editReceipt} name={selectedName} phone={selectedPhone} type={AppoinmentType}/>
+                    :<EditReceipt name={selectedName} phone={selectedPhone} type={AppoinmentType}/>
+            }
+            
                 
                           
                     
-            </div>
+        </div>
     );
     
 }
