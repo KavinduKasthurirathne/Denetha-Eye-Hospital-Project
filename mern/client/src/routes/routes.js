e63366219c11d92e893e6ab6e0c053657ceacf47
it21047824 <it21047824@my.sliit.lk>
2022-10-17 20:28:47 +0530

    fixed routes.js

--- mern/client/src/routes/routes.js
+++ mern/client/src/routes/routes.js
@@ -57,41 +57,4 @@ const createRoutes = () => {
     );
 }
 
-      <Route exact path="/" element={<App />} />
-      <Route exact path="accountant" element={<Accountant />} />
-      <Route exact path="receptionist" element={<Reception />}></Route>
-      <Route exact path="staff" element={<Patients />} />
-      <Route exact path="doctor" element={<Patients />} />
-      <Route exact path="admin" element={<Patients />} />
-      <Route exact path="patient" element={<Patients />} />
-      <Route exact path="AddPatientForm" element={<AddPatientForm />} />
-      <Route exact path="editappointment" element={<EditAppoinment />} />
-      <Route
-        exact
-        path="AppointmentMainpage"
-        element={<AppointmentMainpage />}
-      />
-      <Route exact path="appointment" element={<Appoinment />} />
-      <Route exact path="appoinmenttable" element={<AppoinmentTable />} />
-      <Route exact path="meetings" element={<Meetings />} />
-      <Route exact path="profile" element={<Profile />} />
-      <Route exact path="surgery" element={<RecordList />} />
-      <Route exact path="addNew" element={<AddSuPatient />} />
-      {/* <Route exact path = 'edit' element = {<Edit/>} />
-            <Route exact path = 'diagForm' element = {<DForm/>} /> */}
-      <Route exact path="meetings/add_meetings" element={<AddMeeting />} />
-      <Route exact path="manager/addprofile" element={<AddNewMember />} />
-
-      <Route exact path="editAcc" element={<UserAccount />} />
-      <Route exact path="help" element={<Help />} />
-      <Route exact path="logout" element={<Logout />} />
-      <Route
-        exact
-        path="*"
-        element={<h2 className="App">404 - Page not found!</h2>}
-      />
-    </Routes>
-  );
-};
-
 export default createRoutes;
