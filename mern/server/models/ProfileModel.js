d8092437c759c30b436323370996f2a884f02054
it21047824 <it21047824@my.sliit.lk>
2022-10-19 13:58:10 +0530

    fix login

--- mern/server/models/ProfileModel.js
+++ mern/server/models/ProfileModel.js
@@ -2,15 +2,6 @@
 const Mongoose = require('mongoose');
 
 const profileSchema = new Mongoose.Schema({
-    id: {
-        type : String,
-        require : true
-    },
-
-    name: {
-        type : String,
-        require : true
-    },
 
     contactno: {
         type : String,
@@ -18,11 +9,6 @@ const profileSchema = new Mongoose.Schema({
     },
 
     address: {
-        type : String,
-        require : true
-    },
-
-    jobrole: {
         type : String,
         require : true
     },
