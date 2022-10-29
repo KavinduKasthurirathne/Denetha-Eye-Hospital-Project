bb7c496e286ee29a6361bb5bd877f3f8352f8f55
it21047824 <it21047824@my.sliit.lk>
2022-10-29 17:53:35 +0530

    profile+account merged. transaction page 95%

--- mern/client/src/components/StaffDetails/Profile.js
+++ mern/client/src/components/StaffDetails/Profile.js
@@ -58,53 +58,12 @@ const Profile = () => {
   const backbtn = useNavigate();
   return (
     <div className="mainDiv">
-      <button
-        onClick={() => {
-          backbtn(-1);
-        }}
-        style={{
-          position: "relative",
-          marginTop: "3%",
-          marginLeft: "3%",
-          width: "100px",
-          backgroundColor: "#CBCBCB",
-        }}
-        className="button"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="25"
-          height="25"
-          fill="black"
-          class="bi bi-arrow-left"
-          viewBox="0 0 15 15"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
-          />
-        </svg>
-      </button>
       <div className="updateform">
-        <h3>Profile</h3>
+        <h3>Additional Info</h3>
         <hr />
 
         <div className="updateFormout">
           <form onSubmit={addData}>
-            <div>
-              <label>Username : </label>
-              <input value={cookies.username} disabled />
-
-              <br />
-            </div>
-
-            <label>Name :</label>
-            <input value={cookies.name} disabled />
-            <br />
-
-            <label>Job role :</label>
-            <input value={cookies.role} disabled />
-            <br />
 
             <label>Contact No :</label>
             <input value={contactno} />
@@ -134,15 +93,6 @@ const Profile = () => {
                 setDob(e.target.value);
               }}
               value={dob}
-            />
-            <br />
-
-            <label>Basic Salary :</label>
-            <input
-              onChange={(e) => {
-                setBasicSal(e.target.value);
-              }}
-              value={basicSal}
             />
             <br />
             <center>
