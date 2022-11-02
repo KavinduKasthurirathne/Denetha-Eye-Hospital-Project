273e7f039926f47c1190a55907ae615b844b9b3f
IT21077692 - Kasthurirathne K. K. I <it21077692@my.sliit.lk>
2022-11-03 00:41:13 +0530

    COMMIT

--- mern/client/src/components/PatientProfile.js
+++ mern/client/src/components/PatientProfile.js
@@ -1,12 +1,26 @@
 import '../PatientProfile.css';
 import '../App.css';
-
+import React, { useState } from 'react';
+import { useCookies } from 'react-cookie';
+import { Button, TextField } from '@mui/material';
 
 const printicon = require('../image/print.png');
 
 export const PatientProfile = () => {
 
     function printProfile(e) {
+        alert("Clicked")
+    };
+
+    function deleteProfile(e) {
+        alert("Clicked")
+    };
+
+    function updateProfile(e) {
+        alert("Clicked")
+    };
+
+    function cancel(e) {
         alert("Clicked")
     };
 
@@ -17,45 +31,65 @@ export const PatientProfile = () => {
             <p class='denethahead'><u><b>Denetha Eye Hospital</b></u></p>
             <p class='head'><u><b>Patient Details</b></u></p>
 
-            <table class='table2'>
-                <label for="name">Name: </label>
-                <input type="text" id="name" name="name" required></input> <br></br>
+            <div class="textfield">
+                <TextField
+                    sx={{width:400}}
+                    name='name'
+                    label='Name'
+                    variant='standard'/>
 
-                <label for="age">Age: </label>
-                <input type="text" id="age" name="age" required></input> <br></br>
+                <TextField
+                    sx={{marginTop:2, width:400}}
+                    name='age'
+                    label='Age'
+                    variant='standard'/>
 
-                <label for="gender">Gender (M/F): </label>
-                <input type="text" id="gender" name="gender" required></input> <br></br>
+                <TextField
+                    sx={{marginTop:2, width:400}}
+                    name='gender'
+                    label='Gender'
+                    variant='standard'/>
 
-                <label for="dob">Date of Birth: </label>
-                <input type="date" id="dob" name="dob"></input> <br></br>
-                
-                <label for="address">Address: </label>
-                <input type="text" id="address" name="address"></input> <br></br>
+                <TextField
+                    sx={{marginTop:2, width:400}}
+                    name='dob'
+                    label='Date of Birth'
+                    variant='standard'/>
 
-                <label for="number">Phone Number: </label>
-                <input type="text" id="number" name="number" required></input> <br></br>
+                <TextField
+                    sx={{marginTop:2, width:400}}
+                    name='address'
+                    label='Address'
+                    variant='standard'/>
 
-                <label for="gname">Guardian's Name: </label>
-                <input type="text" id="gname" name="gname"></input> <br></br>
+                <TextField
+                    sx={{marginTop:2, width:400}}
+                    name='phone'
+                    label='Phone Number'
+                    variant='standard'/>
 
-                <label for="gnumber">Guardian's Phone Number: </label>
-                <input type="text" id="gnumber" name="gnumber"></input> <br></br>
+                <TextField
+                    sx={{marginTop:2, width:400}}
+                    name='gname'
+                    label='Guardian Name'
+                    variant='standard'/>
 
+                <TextField
+                    sx={{marginTop:2, width:400}}
+                    name='gphone'
+                    label='Guardian Phone Number'
+                    variant='standard'/>
+            </div>
 
-                <div class='buttons'>
-                    <button id='deleteBtn' className='button' type="delete"><b>Delete</b></button>
-                    <button id='updateBtn' className='button' type="submit"><b>Update</b></button>
-                    <button id='cancelBtn' className='button' type="cancel"><b>Cancel</b></button>
-                </div>
-                
-                <div class='printButton'>
-                    <button id='printBtn' className='button' type='print'>
-                        <img id="redirecting" src={printicon} alt='printicon' className='print-icon' onClick={printProfile}/>
-                    </button>
-                </div>
-
-            </table>
+        
+            <div class='buttonsView'>
+                <button id='deleteBtn' className='button' type="delete" onClick={deleteProfile}><b>Delete</b></button>
+                <button id='updateBtn' className='button' type="submit" onClick={updateProfile}><b>Update</b></button>
+                <button id='cancelBtn' className='button' type="cancel" onClick={cancel}><b>Cancel</b></button>
+                <button id='printBtn' className='button' type='print' onClick={printProfile}>
+                    <img id="redirecting" src={printicon} alt='printicon' className='print-icon'/>
+                </button>
+            </div>
 
         </div>
         
