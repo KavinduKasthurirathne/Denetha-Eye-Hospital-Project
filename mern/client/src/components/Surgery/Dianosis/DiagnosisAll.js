e56307f7e32d1db2ef2cfd585293f45b8f271ebb
Asini Madushka <it21078514@my.sliit.lk>
2022-11-02 11:55:19 +0530

    New Changes

--- mern/client/src/components/Surgery/Dianosis/DiagnosisAll.js
+++ mern/client/src/components/Surgery/Dianosis/DiagnosisAll.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import {useNavigate} from 'react-router-dom';
 import { Link } from "react-router-dom";
-import './SurgeryDetails.css';
+//import './SurgeryDetails.css';
 
  
 const Record = (props) => (
